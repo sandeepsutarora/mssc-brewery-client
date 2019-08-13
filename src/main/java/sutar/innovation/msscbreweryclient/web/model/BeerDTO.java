67a2059884eb5273b512b9bb08e516c55a1d66bf
Sandeep Sutar <sandeepsutarora@gmail.com>
2019-08-13 21:47:38 +0530

    test

--- src/main/java/sutar/innovation/msscbreweryclient/web/model/BeerDTO.java
+++ src/main/java/sutar/innovation/msscbreweryclient/web/model/BeerDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 @Data
@@ -12,8 +16,15 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDTO {
+    @Null
     private UUID id;
+
+    @NotNull
     private String beerName;
+
+    @NotBlank
     private String beerStyle;
+
+    @Positive
     private Long upc;
 }
